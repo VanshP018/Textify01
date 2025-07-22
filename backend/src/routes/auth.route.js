7948cd47996fece00d1d4276a9a87fdaef2e095a
VanshP018 <vanshpanwar500005@gmail.com>
2025-07-22 16:54:44 +0530

    pages done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,12 @@
-import express from "express"
-import {login , signup , logout} from "../controllers/auth.controller.js"
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup" , signup)
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
-router.post("/login" , login)
-
-router.post("/signup" , logout)
-
-
-router.put("/upate-profile" , updateProfile)
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
